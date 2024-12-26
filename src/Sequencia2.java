1afb6439b68487f62cf02690c37ac427ec65294a
Kyoba <bruno-tremarin@hotmail.com>
2024-12-26 12:22:30 -0300

    Sequencia 2

--- src/Sequencia2.java
+++ src/Sequencia2.java
@@ -2,7 +2,27 @@
 public class Sequencia2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		I=1 J=7
+//		I=1 J=6
+//		I=1 J=5
+//		I=3 J=7
+//		I=3 J=6
+//		I=3 J=5
+		
+		int j = 7;
+		int i = 1;
+		
+		while(i<=9) {
+			for(int l = 0; l<3;l++) {
+				System.out.println("I=" + i + " J=" + j);
+				j -= 1;
+			}
+			if(j == 4) {
+				j = 7;
+				i += 2;
+			}
+		}
+		
 
 	}
 
